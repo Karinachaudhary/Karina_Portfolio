9fbf42eb363314b2ac618e3ab1440d627163ae16
Karina Chaudhary <karinachaudharyyy@gmail.com>
2026-08-16 20:24:21 +0545

    environment setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import tailwindcss from 'tailwindcss'
+ export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-400 mb-4">
+        Portfoliooo 🚀
+      </h1>
+    </div>
+  )
+}
